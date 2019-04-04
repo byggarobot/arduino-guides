e45b9472cf28fe97e69d828629de7c23078fa6b5
Andreas Hagelberg <andreas@hagelberg.name>
2019-04-04 23:44:29 +0200

    Add project 004

---
+++ 004-dimmer/004-dimmer.ino
@@ -0,0 +1,20 @@
+// Reservera minne för variabler att lagra data i
+int varde_in;
+int varde_ut;
+
+void setup() {
+  // Ställ om pinne 2 till till att vara en utgång.
+  pinMode(2, OUTPUT);
+  // Ställ om pinne A0 till att vara en ingång.
+  pinMode(A0, INPUT);
+}
+
+
+void loop() {
+  // Läs av vridmotståndets position. Detta ger ett värde mellan 0 och 1023.
+  varde_in = analogRead(A0);
+  // Skala om värdet så att det är mellan 0 och 255.
+  varde_ut = map(varde_in, 0, 1023, 0, 255);
+  // Sätt lysdiodens ljusstyrka.
+  analogWrite(2, varde_ut);
+}
