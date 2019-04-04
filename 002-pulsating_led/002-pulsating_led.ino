78565c472d8a9adbf02cd6738b4f52d374746395
Andreas Hagelberg <andreas@hagelberg.name>
2019-04-04 23:10:21 +0200

    Add code for project 002

---
+++ 002-pulsating_led/002-pulsating_led.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // Set pin 2 to be an output.
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  // Increase the intensity of the LED from completely off to fully lit.
+  for (int i = 0; i < 255; i++) {
+    // Set the intensity of the LED.
+    analogWrite(2, i);
+    // Wait 10ms.
+    delay(10);
+  }
+  // Decrease the intensity of the LED from fully lit to completely off.
+  for (int i = 255; i > 0; i--) {
+    // Set the intensity of the LED.
+    analogWrite(2, i);
+    // Wait 10ms.
+    delay(10);
+  } 
+}
+
