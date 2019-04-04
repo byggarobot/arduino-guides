6af62dfff66349e3fcb5b470f2228f4ae977ae2c
Andreas Hagelberg <andreas@hagelberg.name>
2019-04-04 22:53:23 +0200

    Add project 001

---
+++ 001-blinking_led/001-blinking_led.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // Set pin 2 to be an output.
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on.
+  digitalWrite(2, HIGH);
+  // Wait half a second (500ms).
+  delay(500);
+  // Turn the LED off.
+  digitalWrite(2, LOW);
+  // Wait another half a second before the program restarts.
+  delay(500);
+}
+
