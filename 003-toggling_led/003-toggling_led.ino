e1c143114408ee43d11605e4a838c829ad372bb0
Andreas Hagelberg <andreas@hagelberg.name>
2019-04-04 23:56:21 +0200

    Add missing files for project 003 and 004

---
+++ 003-toggling_led/003-toggling_led.ino
@@ -0,0 +1,29 @@
+// Reserve memory for storing variables in
+int led_state = LOW;
+int button_state;
+
+void setup() {
+  // Set pin 2 to be an output.
+  pinMode(2, OUTPUT);
+  // Set pin 3 to be an input with a pull-up resistor.
+  pinMode(3, INPUT_PULLUP);
+}
+
+
+void loop() {
+  // Read the state of the button.
+  button_state = digitalRead(3);
+  // If the button is pressed the input goes low.
+  if (button_state == LOW) {
+    // Toggle the LED
+    if (led_state == LOW) {
+      led_state = HIGH;
+    }
+    else {
+      led_state = HIGH;
+    }
+    digitalWrite(2, led_state); 
+    // Wait a bit to surpress contact bounce
+    delay(250);
+  }
+}
---
+++ 004-dimmer/004-dimmer.ino
@@ -0,0 +1,20 @@
+// Reserve memory for variables to store data in.
+int value_in;
+int value_out;
+
+void setup() {
+  // Set pin 2 to be an output.
+  pinMode(2, OUTPUT);
+  // Set pi A0 to be an input.
+  pinMode(A0, INPUT);
+}
+
+
+void loop() {
+  // Read the position of the potentiometer. This gives a value between 0 and 1023.
+  value_in = analogRead(A0);
+  // Scale the value so it's between 0 and 255.
+  value_out = map(value_in, 0, 1023, 0, 255);
+  // Set the intensity of the LED.
+  analogWrite(2, value_out);
+}
