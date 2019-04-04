f29fd9cdb625c576643118bb20d0e71bf28e904c
Andreas Hagelberg <andreas@hagelberg.name>
2019-04-04 23:43:27 +0200

    Add project 003

---
+++ 003-toggling_led/003-toggling_led.ino
@@ -0,0 +1,27 @@
+// Reservera minne för variabler att lagra data i
+int lysdiodstatus = LOW;
+int knappstatus;
+
+void setup() {
+  // Ställ om pinne 2 till till att vara en utgång.
+  pinMode(2, OUTPUT);
+  // Ställ om pinne 3 till att vara en ingång med pull-up motstånd
+  pinMode(3, INPUT_PULLUP);
+}
+
+
+void loop() {
+  // Läs in tryckknappens tillstånd.
+  knappstatus = digitalRead(3);
+  // Om knappen trycks ner blir ingången låg (LOW)
+  if (knappstatus == LOW) {
+    if (lysdiodstatus == LOW) {
+      lysdiodstatus = HIGH;
+    }
+    else {
+      lysdiodstatus = HIGH;
+    }
+    digitalWrite(2, LOW); 
+    delay(250);
+  }
+}
